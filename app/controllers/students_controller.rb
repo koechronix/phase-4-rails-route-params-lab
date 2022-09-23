328b970d62927740396b85111210714458c399b4
koechronix <koechronix@yahoo.com>
2022-09-23 10:26:30 +0300

    Completed route lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,12 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    # find students ID by using URL
+    student = Student.find(params[:id])
+    # send json response
+    render json: student
+
+  end
+
 end
